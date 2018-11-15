14f0ead906622ef2e9f80d46a14ffc9f7ef5a41c
Erin <day1ct@cmich.edu>
2018-11-15 18:41:53 -0500

    added classes

---
+++ item.py
@@ -0,0 +1,7 @@
+class item:
+    def __init__(self, name, weight, price, color, quantity):
+        self.name = name
+        self.weight = weight
+        self.price = price
+        self.color = color
+        self.quantity = quantity
