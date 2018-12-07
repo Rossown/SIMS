f28ca445646bc4227175c42a3bf6945c1f3e8ba2
Erin <day1ct@cmich.edu>
2018-12-06 20:42:42 -0500

    documentation!!

--- database.py
+++ database.py
@@ -1,11 +1,17 @@
 import os
 
+#load function
 def load(filename, password):
+    #initialize item list
     itemList = []
+    #make sure our csv folder exists
     if not os.path.exists("csv"):
         return False
+    #attempt to open the corrosponding file
     with open("csv/"+filename+".csv", "r") as file:
+        #skip password line
         next(file)
+        #split each line by comma and add information to itemList as dict
         for line in file:
             line2 = line.split(",")
             itemList.append({
@@ -18,11 +24,16 @@ def load(filename, password):
             })
     return itemList
 
+#save function
 def save(filename, list, password):
+    #verify csv exists
     if not os.path.exists("csv"):
         os.makedirs("csv")
+    #open the corrosponding csv
     with open("csv/"+filename+".csv", "w+") as file:
+        #write our password to csv
         file.write(password+"\n")
+        #write each item to csv with commas separating info
         for item in list:
             file.write(format("%s,%s,%s,%s,%s,%s\n"%(
                 item['id'],
@@ -33,10 +44,13 @@ def save(filename, list, password):
                 item["quantity"]
             )))
 
+#login verification
 def checkLogin(filename, password):
+    #if file does not exist, create a new file for the user with correct password
     if not os.path.exists("csv\\"+filename+".csv"):
         with open("csv\\"+filename+".csv", "w") as file:
             file.write(password)
         return True
+    #if file exists, open it and verify password against first line
     with open("csv\\"+filename+".csv", "r") as file:
         return password.strip() == file.readline().strip()
--- init.py
+++ init.py
@@ -4,61 +4,68 @@ from pprint import pprint
 
 import database
 
-def create_app(test_config=None):
+def create_app(test_config=None): #initialize program
     app = Flask(__name__, instance_relative_config=True)
     app.secret_key = 'fish'
 
-    # app.config.from_mapping(SECRET_KEY='dev', DATABASE="")
-
-    # if test_config is None:
-    #     # load the instance config, if it exists, when not testing
-    #     app.congif.from_pyfile('config.py',silent=True)
-    # else:
-    #     # load the test config if passed in
-    #     app.config.from_mapping(test_config)
-    
-    # # ensure the instance folder exists
+    #ensure we have the correct file pathings
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
+    #index page
     @app.route('/')
     def index():
-        if 'name' not in session.keys() or 'password' not in session.keys():
+        #check if user has logged in
+        if 'name' not in session.keys() or 'password' not in session.keys() or 'name'=='':
             return redirect("/login")
+        #check if items list has been initialized
         if 'items' not in session.keys() or session['items'] == None:
             print("reset items")
             session['items'] = []
         session.modified = True
+        #display page
         return render_template('index.html', item_list = session['items'], name = session['name'])
 
+    #login page
     @app.route("/login", methods = ['GET', 'POST'])
     def login():
+        #taking in a login request
         if request.method == 'POST':
-            if(database.checkLogin(request.form['name'], request.form['password'])):
+            #verify request from database
+            if(request.form['name'] != '' and database.checkLogin(request.form['name'], request.form['password'])):
                 session['name'] = request.form['name']
                 session['password'] = request.form['password']
                 data = database.load(session['name'], session['password'])
+                #verify loading of data
                 if(data != False):
                     session['items'] = database.load(session['name'], session['password'])
                     session.modified = True
                     return redirect('/')
             return redirect('/login')
+        #reset name in session
         if 'name' in session.keys():
             del session['name']
+        #reset password in session
         if 'password' in session.keys():
             del session['password']
+        #display page
         return render_template('Login.html')
 
+    #add item page
     @app.route("/add", methods = ['GET', 'POST'])
     def add():
+        #verify logged in
         if 'name' not in session.keys() or 'password' not in session.keys():
             return redirect("/login")
+        #take in item additions
         if request.method == 'POST':
+            #get highest id to prevent repetition
             maxId = 0
             for item in session['items']:
                 maxId = max(item['id'], maxId)
+            #create item dictionary for local save
             dic = {
                 "id":maxId+1,
                 "name":request.form["name"],
@@ -67,31 +74,46 @@ def create_app(test_config=None):
                 "color":request.form["color"],
                 "quantity":request.form["quantity"],
             }
+            #save item to session
             session['items'].append(dic)
             session.modified = True
             return redirect('/')
+        #display input form
         return render_template('Form.html')
 
+    #delete item page
     @app.route("/delete/<id>")
     def delete(id):
+        #verify logged in
         if 'name' not in session.keys() or 'password' not in session.keys():
             return redirect("/login")
+        #check for correct id, and delete first if exists
         for i in range(len(session['items'])):
             if(str(session['items'][i]['id']) == id):
                 del session['items'][i]
                 break
+        #return to index
         session.modified = True
         return redirect('/')
 
+    #print page
     @app.route('/print')
-    def printReport():        
+    def printReport():  
+        #display printable page      
         return render_template('Report.html', item_list = session['items'])
     
+    #modify page
     @app.route("/modify/<id>", methods = ['GET', 'POST'])
     def details(id):
+        #verify login
+        if 'name' not in session.keys() or 'password' not in session.keys():
+            return redirect("/login")
+        #take inputs
         if request.method == 'POST':
+            #search for correct item by id
             for i in range(len(session['items'])):
                 if(str(session['items'][i]['id']) == id):
+                    #replace item with new dictionary with new information
                     dic = {
                         "id":id,
                         "name":request.form["name"],
@@ -102,24 +124,31 @@ def create_app(test_config=None):
                     }
                     session['items'][i] = dic
                     break
+            #modify and return to index
             session.modified = True
             return redirect('/')
+        #search for correct item by id, display item's information on modify page
         for i in range(len(session['items'])):
             if(str(session['items'][i]['id']) == id):
                 return render_template('DetailsPage.html', item = session['items'][i])
+        #return to index (should never happen)
         return redirect('/')
     
+    #save page
     @app.route("/save")
     def save():
+        #verify login
         if 'name' not in session.keys() or 'password' not in session.keys():
             return redirect("/login")
+        #call database save function
         database.save(session['name'], session['items'], session['password'])
+        #return to index
         return redirect("/")
 
+    #send application with functions to client
     return app
-   
 
-
+#create application in main method
 if __name__ == '__main__':
     app = create_app()
     app.run()
